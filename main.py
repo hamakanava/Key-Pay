65828351c535b1a22aa91772a6dcc84387b61fd5
hamakanava <hamikanava@yandex.ru>
2023-06-23 10:40:03 +0500

    Auto build APK file

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 from kivy.lang import Builder
-
 from kivymd.app import MDApp
-
+from kivymd.uix.label import MDLabel
 
 class KeyPay(MDApp):
-
     def build(self):
         self.theme_cls.material_style = "M3"
         # self.theme_cls.theme_style = "Dark"
-        return Builder.load_string(
-            '''
+        return Builder.load_string('''
 MDScreen:
-
     MDBottomNavigation:
-        #panel_color: "#eeeaea"
-        selected_color_background: "orange"
+        selected_color_background: "green"
         text_color_active: "lightgrey"
+        text_color_normal: 0, 0, 0, 1
+        icon_color_normal: 0, 0, 0, 1
 
         MDBottomNavigationItem:
             name: 'screen 1'
             text: 'Главная'
             icon: 'home'
             badge_icon: ""
-
             MDLabel:
                 text: 'Вы в главном меню'
                 halign: 'center'
@@ -32,13 +28,9 @@ MDScreen:
             text: 'Профиль'
             icon: 'account-box'
             badge_icon: ""
-
             MDLabel:
                 text: 'Ваш профиль'
                 halign: 'center'
-
-'''
-        )
-
+''')
 
 KeyPay().run()
